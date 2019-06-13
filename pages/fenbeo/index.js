3140a88a290dc694b1d64e285143fcb46434eeda
chinanumber1 <bzeracn@gmail.com>
2019-06-13 09:48:25 +0800

    xiao

---
+++ pages/fenbeo/index.js
@@ -0,0 +1,78 @@
+// pages/fenbeo/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var pages = getCurrentPages()
+    console.log(pages[0].route)
+    if (pages[0].route == 'pages/fenbeo/index'){
+      pages[0].route = '../../changce/merch/index'
+      wx.navigateTo({
+        url: '../../changce/merch/index'
+      })
+    } else {
+      pages[0].route = 'pages/fenbeo/index'
+      wx.switchTab({
+        url: '/pages/index/index',
+      });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
